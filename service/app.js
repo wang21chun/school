9336cfe8c9e124f807d9ca2b5312239a654fc2dd
wangchun8 <wangchun8@jd.com>
2020-09-12 16:27:13 +0800

    test:　test

--- service/app.js
+++ service/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser')
 const cors = require('cors');
 var path = require('path');
 var cookie = require('cookie');
@@ -22,6 +23,7 @@ app.use(cors({ credentials: true, origin: true }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.text({ type: 'text/html' }))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -60,7 +62,7 @@ app.use(auth);
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/order', orderRouter);
-app.use('/api/course',course);
+app.use('/api/course', course);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
